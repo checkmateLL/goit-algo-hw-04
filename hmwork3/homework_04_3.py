8cd2fc5750ef46d741448ad4bf2263a6828f693b
checkmateLL <saoirse.girl47@gamil.com>
2024-07-01 19:19:55 +0300

    update task 3

--- hmwork3/homework_04_3.py
+++ hmwork3/homework_04_3.py
@@ -5,12 +5,20 @@ from pathlib import Path
 path = sys.argv[1]
 parent_folder_path = Path(path)
 
-def parse_folder(path):
-    for element in path.iterdir():
-        if element.is_dir():
-            print(Fore.YELLOW + f"Parsed data: This is folder - {element.name}")
-            parse_folder(element)
-        if element.is_file():
-            print(Fore.GREEN + f"Parsed data: This is file - {element.name}")
+def parse_folder(path, level=0):
+    try:
+        for element in path.iterdir():
+            indent = " " * 4 * level
+            if element.is_dir():
+                print(Fore.YELLOW + f"{indent}|--{element.name}")
+                parse_folder(element, level + 1)
+            if element.is_file():
+                print(Fore.GREEN + f"{indent}|--{element.name}")
+    except FileNotFoundError:
+        print(Fore.RED + f"Directory {path} not found!")
+    except Exception as e:
+        print(Fore.RED + f"Error: {e}")
+
+
 
 parse_folder(parent_folder_path)
